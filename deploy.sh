b71dae46bf2c3fd8828b1d760dcec675f3ad8973
MovingToWeb <soleil.fils.88@gmail.com>
2019-08-17 10:41:53 +0530

    [add] spring boot project

---
+++ deploy.sh
@@ -0,0 +1,3 @@
+echo "#####################"
+echo "Deployed successfully"
+echo "#####################"
---
+++ travis-integration/src/main/java/com/example/travisintegration/TravisIntegrationApplication.java
@@ -0,0 +1,13 @@
+package com.example.travisintegration;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TravisIntegrationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TravisIntegrationApplication.class, args);
+	}
+
+}
